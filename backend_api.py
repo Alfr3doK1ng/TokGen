40991b19d81fae2f2b8c10581516eff1ccc33d8c
Laurie <hluo@hmc.edu>
2024-02-03 14:07:57 -0800

    tiktok api updated

--- backend_api.py
+++ backend_api.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
 from typing import List
 import time
+from dotenv import load_dotenv
+import os
+from apify_client import ApifyClient
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
 
 @dataclass
 class QueryResponse:
@@ -8,5 +14,27 @@ class QueryResponse:
     related_videos: List[str]
 
 def query(q: str) -> QueryResponse:
-    time.sleep(20)
-    return QueryResponse("this is a response", [])
+    return get_video(q)
+
+def get_video(q: str):
+    # Access the API key
+    client = ApifyClient(api_key)
+    run_input = {
+        "type": "SEARCH",
+        "keyword": q,
+        "sortType": 0,
+        "publishTime": "ALL_TIME",
+        "limit": 20,
+        "proxyConfiguration": {
+            "useApifyProxy": False
+        }
+    }
+    # Run the Actor and wait for it to finish
+    run = client.actor("nCNiU9QG1e0nMwgWj").call(run_input=run_input)
+    # Fetch and print Actor results from the run's dataset (if there are any)
+    for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+        print(item)
+        return item
+
+# # example usage
+# query('viral')
